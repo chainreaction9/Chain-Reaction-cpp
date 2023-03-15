a705f56c9f16c98c75f5b3aa8992f62b8ad691e1
chainreaction9 <chain.reaction.server@gmail.com>
2023-03-15 19:43:50 +0100

    Added comments.

--- DefaultOptions.cmake
+++ DefaultOptions.cmake
@@ -15,8 +15,7 @@ option(BUILD_SHARED_LIBS "Build external shared/static libraries" OFF)
 ###  If the following options are turned off, the targetted dependencies won't be downloaded.
 ###  However, they are mandatory requirements for this project.
 ###  So please make sure that the dependencies can be found by a find_package call in config mode
-###  before turning the options off. Git is also a mandatory requirement for some (glew and OpenAL)
-###  of the downloads to work.
+###  before turning the options off.
 option(GLM_AUTO_DOWNLOAD "Download glm dependency via FetchContent_MakeAvailable" ON)
 option(GLEW_AUTO_DOWNLOAD "Download glew dependency via FetchContent_MakeAvailable" ON)
 option(WXWIDGETS_AUTO_DOWNLOAD "Download wxWidgets dependency via FetchContent_MakeAvailable" ON)
